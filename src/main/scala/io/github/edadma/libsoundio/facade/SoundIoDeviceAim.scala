d4f5b2c9a182ab9f54c089ec4ac97cf01678fa98
Edward A Maxedon, Sr <edadma@gmail.com>
2021-08-28 15:02:55 -0400

    start coding facade

--- src/main/scala/io/github/edadma/libsoundio/facade/SoundIoDeviceAim.scala
+++
@@ -1,5 +0,0 @@
-package io.github.edadma.libsoundio.facade
-
-import scala.scalanative.unsafe._
-
-class SoundIoDeviceAim
---
+++ src/main/scala/io/github/edadma/libsoundio/facade/package.scala
@@ -0,0 +1,5 @@
+package io.github.edadma.libsoundio
+
+import scala.scalanative.unsafe._
+
+package object facade {}
